987bd56274794e8c3e30894f49ce7d951775a663
Daniel Schneider <daschne8@gmail.com>
2019-03-11 17:28:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
 
 def remove_strawberry#(contacts)
   binding.pry
-  puts "contacts"
+  puts contacts
   #contacts["Freddy Mercury"][favorite_ice_cream_flavors:].delete("strawberry")
 end
 
